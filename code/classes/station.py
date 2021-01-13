371ade15eec84f14a95c1a65d5dd4f490cb6b5b4
jarih <jari.hoffmann@gmail.com>
2021-01-13 11:13:44 +0100

    added comments to station.py

--- code/classes/station.py
+++ code/classes/station.py
@@ -1,6 +1,8 @@
 class Station():
+    """
+    contains a station with coordinates and connections
+    """
     def __init__(self, name, station_id, x_coord, y_coord):
-        
         self.name = name
         self.station_id = station_id
         self.x_coord = x_coord
@@ -8,9 +10,15 @@ class Station():
         self.connections = {}
 
     def add_connection(self, station, length):
+        """
+        add a connection to the station
+        """
         self.connections[station.station_id] = [station, length]
 
     def get_connections(self):
+        """
+        return a list of all connected station objects
+        """
         return list(self.connections.values())
 
     def __repr__(self):
