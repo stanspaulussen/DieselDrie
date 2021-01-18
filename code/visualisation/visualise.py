609b220aa1000ba5d1a39cefca5f31e71c9554c6
Pauline van Lieshout <paulinevanlieshout@MacBook-Air-van-Pauline.local>
2021-01-18 13:20:41 +0100

    visualise updated cleaned

--- code/visualisation/visualise.py
+++ code/visualisation/visualise.py
@@ -1,53 +1,77 @@
+"""
+visualise.py
+Minor Programming: Theory
+By: Pauline van Lieshout, Jari Hoffman and Stans Paulussen
+
+This file allows for visualisation of the traintracks.
+
+X and y coordinates are swapped in the input file and corrected in this file. 
+"""
+
+
 import matplotlib.pyplot as plt
 from code.classes.grid import Grid
 import numpy as np
 
+
 def graph(grid):
+    """
+    Creates a graph based on the current state of the grid stations, connections and tracks.
+    """
     
+    # retrieves stations and tracks 
     stations = grid.stations.values()
     tracks = grid.tracks.values()
-    colors = ['.:k', '.-r', '.:g', '.-b', '.:y', '.-m', '.:c']
-    line_widths = [8, 7, 6, 5, 4, 3, 2]
-    cursor = 0
 
-    # get coordinates 
+    # gets coordinates of stations
     for station in stations:
+
+        # gets coordinates of the first station of a connection (station A)
+        x_A = float(station.y_coord)
+        y_A = float(station.x_coord)
 
         connections = station.connections 
 
-        y_A = float(station.x_coord)
-        x_A = float(station.y_coord)
-
+        # gets coordinates of the second station of a connection (station B)
         for connection in connections:
             
             destination = grid.stations.get(connection)
 
-            y_B = float(destination.x_coord)
             x_B = float(destination.y_coord)
+            y_B = float(destination.x_coord)
 
+            # plots the stations and their connections in graph
             plt.scatter([x_A, x_B], [y_A, y_B])
-            
             plt.plot([x_A, x_B], [y_A, y_B], '.-k')
             plt.text(x_A, y_A, station.name)
             plt.text(x_B, y_B, destination.name)
 
+    # adds styling to track plot 
+    colors = ['.:k', '.-r', '.:g', '.-b', '.:y', '.-m', '.:c']
+    line_widths = [8, 7, 6, 5, 4, 3, 2]
+    cursor = 0
+
+    # goes through tracks
     for track in tracks:
-        print("Hier komt de track")
-        print(track)
-        color = colors[cursor]
-        line_width = line_widths[cursor]
+
+        # styling of tracks
+        color = colors[cursor % 7]
+        line_width = line_widths[cursor % 7]
         cursor += 1
 
         track_list = list(track.stations.values())
 
+        # goes through track connections
         for i in range(len(track_list)):
             if (i + 1) in range(len(track_list)):
-                y_A = float(track_list[i].x_coord)
+
+                # gets coordinates of track connections
                 x_A = float(track_list[i].y_coord)
-                
-                y_B = float(track_list[i + 1].x_coord)
+                y_A = float(track_list[i].x_coord)
                 x_B = float(track_list[i + 1].y_coord)
- 
+                y_B = float(track_list[i + 1].x_coord)
+                
+                # plots tracks
                 plt.plot([x_A, x_B], [y_A, y_B], color, alpha=0.7, linewidth=line_width)
             
     plt.show()
